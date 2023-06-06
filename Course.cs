ff2f7b4fe1c53456cfd342ab0a874f104e274ded
Katia.Shvachko <katya.shvachko@gmail.com>
2023-06-06 22:14:45 +0300

    Add project files.

---
+++ Course.cs
@@ -0,0 +1,34 @@
+﻿namespace MyNamespace
+{
+    class Course
+    {
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+        public int CourseDuration { get; set; }
+        public int NumberOfStudents { get; set; }
+
+        public Course(string courseName, string teacherName, int courseDuration, int numberOfStudents)
+        {
+            CourseName = courseName;
+            TeacherName = teacherName;
+            CourseDuration = courseDuration;
+            NumberOfStudents = numberOfStudents;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Course Name: {CourseName}");
+            Console.WriteLine($"Teacher: {TeacherName}");
+            Console.WriteLine($"Duration: {CourseDuration} hours");
+            Console.WriteLine($"Number of Students: {NumberOfStudents}");
+        }
+
+        public void AddStudent()
+        {
+            NumberOfStudents++;
+        }
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a Student object
+            List<string> attendedCourses = new List<string> { "Живопис", "Малюнок" };
+            Student student = new Student("Vincent", "Van Gogh", 20, "Amsterdam", attendedCourses);
+
+            // Creating a Course object
+            Course course = new Course("Анатомія", "Leonard de Vinci", 40, 10);
+
+            // Printing student and course information
+            student.Print();
+            Console.WriteLine();
+            course.Print();
+
+            // Adding a course to the student
+            student.AddCourse("Анатомія");
+            Console.WriteLine("\nAfter adding a course:");
+            student.Print();
+
+
+            // Adding a student to the course
+            course.AddStudent();
+            Console.WriteLine("\nAfter adding a student to the course:");
+            course.Print();
+
+
+            // Deleting a course from the student
+            student.DeleteCourse("Анатомія");
+            Console.WriteLine("\nAfter deleting a course:");
+            student.Print();
+        }
+    }
+}
